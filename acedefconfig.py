d02f37adea2a458c6e9f74473df28cf632f2fe67
Jorge Ceballos <jorgeceballosgarcia@gmail.com>
2026-02-06 11:30:12 +0100

    Update acedefconfig.py

--- acedefconfig.py
+++ acedefconfig.py
@@ -39,6 +39,7 @@ class AceDefConfig(object):
     #transcodecmd['default'] = 'ffmpeg -i - -c:a copy -c:v copy -f mpegts -'.split()
     videoseekback = 0
     videotimeout = 30
+    use_chunked = True  # Enable HTTP/1.1 chunked transfer encoding for streaming
     fakeuas = ('Mozilla/5.0 IMC plugin Macintosh', )
     loglevel = logging.DEBUG
     logfmt = '%(filename)-20s [LINE:%(lineno)-4s]# %(levelname)-8s [%(asctime)s]  %(message)s'
